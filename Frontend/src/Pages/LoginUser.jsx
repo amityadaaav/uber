a95ea002ba8784fbc74617bb5f6d315ae1c61a19
Amityadaaav <amityadaaav1234@gmail.com>
2025-11-12 17:40:02 +0530

    latest one

--- Frontend/src/Pages/LoginUser.jsx
+++ Frontend/src/Pages/LoginUser.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const LoginUser = () => {
   return (
-    <div>
-      
+    <div className='p-7 h-screen flex justify-center items-center'>
+       <img className="w-16 ml-8" src="https://www.logo.wine/a/logo/Uber/Uber-Logo.wine.svg" alt="" />
+
+      <form action="">
+        <h2>Login User</h2>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" />
+          <label htmlFor="email">Password:</label>
+          <input type="password" /> 
+          </div>
+      </form>
     </div>
   )
 }
